cc76fe474c8ecc6111b760ba8779b3bf2b7f7c28
Piotr Barczuk <piotr.wiktor.barczuk@gmail.com>
2012-06-26 15:35:12 +0200

    modified API url, version 0.0.11

--- lib/multiinfo/api/command.rb
+++ lib/multiinfo/api/command.rb
@@ -2,7 +2,6 @@ require "cgi"
 module MultiInfo
   class API
     API_SERVICE_HOST = 'https://api1.multiinfo.plus.pl/'
-    API_NAME = 'smsapi4'
     API_SCRIPT_TYPE = 'aspx'
     
     class Command
@@ -13,7 +12,7 @@ module MultiInfo
   
       def with_params(param_hash)
         param_string = '?' + param_hash.map { |key, value| "#{::CGI.escape(key.to_s.camelize(false))}=#{::CGI.escape(value.to_s)}" }.sort.join('&')
-        URI.parse(File.join("#{API_SERVICE_HOST}#{API_NAME}/", "#{@command_name}.#{API_SCRIPT_TYPE}" + param_string))
+        URI.parse(File.join("#{API_SERVICE_HOST}", "#{@command_name}.#{API_SCRIPT_TYPE}" + param_string))
       end
       
     end
